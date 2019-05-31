dc8b8f3a3d317f2cc227e208349356f75d6c2d85
Anita Chan <achan@and.digital>
2019-05-31 10:56:03 +0100

    Created Navbar component, restructured folders + favicon.icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import Heading from './Heading//Heading'
+import Heading from "./Heading//Heading";
+import NavBar from "./NavBar/NavBar";
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Heading test="welcome"/>
+        <NavBar />
+        <Heading />
       </div>
     );
   }
---
+++ src/HomePage/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./NavBar.scss";
+import { ReactComponent as Logo } from "../../assets/Logo.svg";
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <div className="Logo" />
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About Me</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Contact Me</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;

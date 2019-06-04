4e80ed2ca3bb5f546420dffc216f6493ed9eb0dd
Anita Chan <achan@and.digital>
2019-06-04 16:28:35 +0100

    Adding Skills bar - wip

---
+++ src/HomePage/Skills/Skills.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Skills.scss";
+
+const Skills = () => {
+  return (
+    <div className="skills">
+      <div>
+        <i className="icons devicon-github-plain" />
+      </div>
+      <div>
+        <i className="icons devicon-html5-plain" />
+      </div>
+      <div>
+        <i className="icons devicon-javascript-plain" />
+      </div>
+      <div>
+        <i className="icons devicon-react-original" />
+      </div>
+      <div>
+        <i className="icons devicon-webpack-plain" />
+      </div>
+      <div>
+        <i className="icons devicon-css3-plain" />
+      </div>
+      <div>
+        <i className="icons devicon-gitlab-plain" />
+      </div>
+      <div>
+        <i className="icons devicon-sass-original" />
+      </div>
+    </div>
+  );
+};
+
+export default Skills;

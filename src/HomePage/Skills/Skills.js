cf568a7f3bd11837e7f6588fe719f0da993c4785
Anita Chan <achan@and.digital>
2019-06-05 16:58:21 +0100

    1st attempt to iterate, not the best key value so need to change

--- src/HomePage/Skills/Skills.js
+++ src/HomePage/Skills/Skills.js
@@ -1,33 +1,23 @@
 import React from "react";
 import "./Skills.scss";
 
+const icons = [
+  "icons devicon-github-plain",
+  "icons devicon-html5-plain",
+  "icons devicon-javascript-plain",
+  "icons devicon-react-original",
+  "icons devicon-webpack-plain",
+  "icons devicon-css3-plain",
+  "icons devicon-gitlab-plain",
+  "icons devicon-sass-original"
+];
+
 const Skills = () => {
   return (
     <div className="skills">
-      <div>
-        <i className="icons devicon-github-plain" />
-      </div>
-      <div>
-        <i className="icons devicon-html5-plain" />
-      </div>
-      <div>
-        <i className="icons devicon-javascript-plain" />
-      </div>
-      <div>
-        <i className="icons devicon-react-original" />
-      </div>
-      <div>
-        <i className="icons devicon-webpack-plain" />
-      </div>
-      <div>
-        <i className="icons devicon-css3-plain" />
-      </div>
-      <div>
-        <i className="icons devicon-gitlab-plain" />
-      </div>
-      <div>
-        <i className="icons devicon-sass-original" />
-      </div>
+      {icons.map(icon => (
+        <div key={icon} className={icon} />
+      ))}
     </div>
   );
 };

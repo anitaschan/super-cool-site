a1d7ef4cceee46222b0ff8ab94dde74847192eec
Anita Chan <achan@and.digital>
2019-06-30 16:25:45 +0100

    Basic form validation added in

--- src/ContactPage/Contact.js
+++ src/ContactPage/Contact.js
@@ -1,18 +1,117 @@
 import React from "react";
 import "./Contact.scss";
 
-const Contact = () => {
-  return (
-    <div className="contact-section">
-      <h1 className="contact-section__title">Contact Me</h1>
-      <form className="contact-section__form">
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="email" placeholder="Email" />
-        <input type="text" name="message" placeholder="Message" />
-        <button className="contact-section__submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      errors: {
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+      }
+    };
+  }
+
+  handleChange = event => {
+    event.preventDefault();
+    const validEmailRegex = RegExp(
+      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    );
+
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case "name":
+        errors.name = value.length < 5 ? "Please enter your full name!" : "";
+        break;
+      case "email":
+        errors.email = validEmailRegex.test(value) ? "" : "Email is not valid!";
+        break;
+      case "subject":
+        errors.subject = value.length < 3 ? "Please enter a subject!" : "";
+        break;
+      case "message":
+        errors.message = value.length < 5 ? "Please enter a message" : "";
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+
+    // console.log(this.state);
+  };
+
+  hasErrors = () => {
+    const { name, email, subject, message } = this.state.errors;
+    return name.length > 0 && email.length > 0 && subject > 0 && message > 0;
+  };
+
+  handleSubmit = event => {
+    alert("submitted");
+    event.preventDefault();
+  };
+
+  render() {
+    let hasErrors = this.hasErrors();
+    return (
+      <div className="contact-section">
+        <div className="contact-section__border">
+          <h1 className="contact-section__title">Get in touch!</h1>
+          <form
+            className="contact-section__form"
+            method="post"
+            action="mailto:hello@anitachan.dev"
+            onSubmit={this.handleSubmit}
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              maxLength="20"
+              onChange={this.handleChange}
+            />
+            <div>{this.state.errors.name}</div>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              maxLength="20"
+              onChange={this.handleChange}
+            />
+            <div>{this.state.errors.email}</div>
+            <input
+              type="text"
+              name="subject"
+              placeholder="Subject"
+              onChange={this.handleChange}
+            />
+            <div>{this.state.errors.subject}</div>
+            <textarea
+              className="contact-section__message"
+              placeholder="Message"
+              name="message"
+              onChange={this.handleChange}
+            />
+            <div>{this.state.errors.message}</div>
+            <input
+              disabled={hasErrors}
+              type="submit"
+              className="contact-section__submit"
+              value="Submit"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Contact;

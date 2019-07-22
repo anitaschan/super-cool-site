ef44d75fdcbd6a524c56b20820e83f46501b1a0a
Anita Chan <achan@and.digital>
2019-07-22 15:42:39 +0100

    Centered NavBar, added hover state and changed Logo colour

--- src/HomePage/NavBar/NavBar.js
+++ src/HomePage/NavBar/NavBar.js
@@ -5,18 +5,21 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="Logo" />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact Me</Link>
-        </li>
-      </ul>
+      <Link to="/" className="navbar__link">
+        Home
+      </Link>
+      <Link to="/portfolio" className="navbar__link">
+        Portfolio
+      </Link>
+      <div>
+        <a href="/" className="Logo" />
+      </div>
+      <Link to="/blog" className="navbar__link">
+        Blog
+      </Link>
+      <Link to="/contact" className="navbar__link">
+        Contact Me
+      </Link>
     </nav>
   );
 };

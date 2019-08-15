466150340ff5c42d4d3c8b6d186e2a0334a721cc
Anita Chan <achan@and.digital>
2019-08-15 22:49:24 +0100

    Added in placeholder for blog

--- src/Blog/BlogPlaceholder.js
+++ src/Blog/BlogPlaceholder.js
@@ -4,11 +4,13 @@ import "./BlogPlaceholder.scss";
 const BlogPlaceholder = () => {
   return (
     <Fragment>
+        <div className="background">
       <div className="container">
         <div className="placeholder-logo" />
         <p className="placeholder-text">
           Have some tea, take a break, this page is still in construction
         </p>
+      </div>
       </div>
     </Fragment>
   );
